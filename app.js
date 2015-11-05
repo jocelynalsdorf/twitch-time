97dcf273817b16b5a5ad97a1d69928e1e90f9763
jocelynalsdorf <jfrodsla@gmail.com>
2015-11-05 12:07:42 -0800

    add search-filter that hides all but matching query; add reset list btn that resets viewing of all items on click

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 $(document).ready(function(){
-           });
+          
+  $(document).keypress(function(e) {
+    if(e.which == 13) {
+       var filter = $('#sample6').val();
+
+    // Loop through the comment list
+      $(".individual-item").each(function(item){
+        console.log(item);
+          // If the list item does not contain the text phrase fade it out
+          if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+              $(this).fadeOut();
+
+          // Show the list item if the phrase matches 1
+          } else {
+              $(this).show();
+              
+          }
+      });
+    }
+  });
+  $("#reset-filter").on("click", function(){
+    $(".individual-item").show();
+    alert("clicked");
+  })
+
+});
+
+   
